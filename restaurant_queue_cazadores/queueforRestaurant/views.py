5e18662e43a0b1538addfccb356835b3c15c9eb7
juan arango <ja1254@usnh.edu>
2023-11-08 17:45:42 -0500

    learned how to count how many people we have in the queue and then make that a list that will be used to determine the wait time maybe, and will be used to call the first person in line

--- restaurant_queue_cazadores/queueforRestaurant/views.py
+++ restaurant_queue_cazadores/queueforRestaurant/views.py
@@ -1,7 +1,9 @@
+
 from django.shortcuts import render, redirect
 from .models import Reservation
 from django.http import HttpResponse
 from twilio.rest import Client
+from django.db.models import Count
 
 def index(request):
     return render(request, 'queueforRestaurant/index.html')
@@ -32,6 +34,26 @@ def send_msm(name, phoneNumber):
         body = f'Hello {name}, thank you for joining the waitlist',
         to = f'{phoneNumber}'
     )
+
+def estimate_time():
+
+    peopleInQueue = Reservation.objects.annotate(numberOfPeople=Count("name")) # count the names in the reservation
+    peopleCount = len(peopleInQueue) # get how many reservetions we have
+
+    lstOfPeople = []
+
+    for names in range(peopleCount):
+
+        lstOfPeople.append(peopleInQueue[names].name) # make a list of the people in the queue exit
+    #get the ammount of people in the queue
+    #if there is one table waiting for 4 or less set it for 25 min wait
+    # increment by 10 minutes each table of 4 or less
+    # if the table is 5  then 45 minutes is the wait if busy 
+    # if all tables are taken and there is a party of like 6+ then the wait is 1 hour maybe less
+    
+
+if __name__ == "__main__":
+    estimate_time()
 
 
 
